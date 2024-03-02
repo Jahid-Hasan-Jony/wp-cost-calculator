e854d9356731ddfc2a332da133e48d2f2eb11a34
Jahid-Hasan-Jony <jonyhossain5588@gmail.com>
2024-03-02 16:33:54 +0600

    adding menu page

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
-echo "Hello";
-?>
+
+/*
+ * Plugin Name: Wp Cost Calculator
+ */
+
+
+
+// Added manu page
+
+add_action( 'admin_menu', 'wp_cost_calculator_menu_page' );
+
+
+function wp_cost_calculator_menu_page() {
+    add_menu_page(
+        'WP Cost Calculator',
+        'Cost Calculator',
+        'manage_options',
+        'wp-cost-calculator',
+        'wp_cost_calculator',
+        'dashicons-calculator',
+        20
+    );
+}
+
+function wp_cost_calculator(){
+
+    include_once plugin_dir_path(__FILE__) . 'menu_page.php';
+
+}
